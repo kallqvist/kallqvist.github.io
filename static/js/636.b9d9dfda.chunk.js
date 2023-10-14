cfc46270d86dee062f4fb060bffcac5013f9be55
kallqvist <kallqvist@gmail.com>
2023-10-14 12:53:56 +0200

    build

--- static/js/636.b9d9dfda.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[636],{5636:function(e,u,n){n.r(u),n.d(u,{RouteDeck:function(){return t}});var c=n(184);function t(){return(0,c.jsx)("div",{className:"route deck",children:"DECK"})}u.default=t}}]);
-//# sourceMappingURL=636.b9d9dfda.chunk.js.map
