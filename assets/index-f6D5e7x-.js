a9c95a4d9464b2cd4f0e3136d16ec2cf9a6e5b32
kallqvist <kallqvist@gmail.com>
2024-03-20 06:34:49 +0100

    update build

--- assets/index-f6D5e7x-.js
+++ assets/index-f6D5e7x-.js
@@ -1,4 +1,4 @@
-import{r as Oe,b as D0,j as jn}from"./index-mlTOiQpz.js";/**
+import{r as Oe,b as D0,j as jn}from"./index-f_HU91Jt.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
