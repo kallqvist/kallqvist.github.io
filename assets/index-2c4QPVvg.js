2059ffd06e213ec2e3d4bfb47cc499b2c89f6962
klas <kallqvist@gmail.com>
2024-03-23 18:19:28 +0100

    update build

--- assets/index-2c4QPVvg.js
+++ assets/index-2c4QPVvg.js
@@ -1,4 +1,4 @@
-import{r as Oe,b as D0,j as jn}from"./index-UYsiQFoh.js";/**
+import{r as Oe,b as D0,j as jn}from"./index-i3b4oVmR.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
