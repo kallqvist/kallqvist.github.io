786fbb1937f127ebd094c1b8d960b2a5635ef288
kallqvist <kallqvist@gmail.com>
2024-03-24 15:38:16 +0100

    update build

--- assets/index-foke5Nno.js
+++ assets/index-foke5Nno.js
@@ -1,4 +1,4 @@
-import{r as Oe,b as D0,j as jn}from"./index-p_kRe4cG.js";/**
+import{r as Oe,b as D0,j as jn}from"./index-ZTnS-oeh.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
