02280459f2bdfa5392a0bf64194b3d1b764521ec
kallqvist <kallqvist@gmail.com>
2024-03-24 08:23:43 +0100

    update build

--- assets/index-Ubo0CaTR.js
+++ assets/index-Ubo0CaTR.js
@@ -1,4 +1,4 @@
-import{r as Oe,b as D0,j as jn}from"./index-piCkZc4G.js";/**
+import{r as Oe,b as D0,j as jn}from"./index-Y-d9HoOJ.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
