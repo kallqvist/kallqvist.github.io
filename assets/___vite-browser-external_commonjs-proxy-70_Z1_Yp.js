47e32b6701b27014113c2068e8a4cdf8e5d7286e
kallqvist <kallqvist@gmail.com>
2024-03-27 11:56:34 +0100

    update build

--- assets/___vite-browser-external_commonjs-proxy-70_Z1_Yp.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index-XiIlW2la.js";const t={},r=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a=e(r);export{a as r};
---
+++ assets/___vite-browser-external_commonjs-proxy-R86LWISS.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-O0Fl2C7K.js";const t={},r=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a=e(r);export{a as r};
