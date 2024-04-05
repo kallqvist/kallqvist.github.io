52738d61bf2a220a75cc8f6f6c3fa7566ecd8f7a
kallqvist <kallqvist@gmail.com>
2024-04-05 21:25:19 +0200

    update build

---
+++ assets/___vite-browser-external_commonjs-proxy-IwDAp-fy.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-LpZb3aPR.js";const t={},r=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a=e(r);export{a as r};
--- assets/index-T5gA2-zO.js
+++ assets/index-T5gA2-zO.js
@@ -1,4 +1,4 @@
-import{r as Oe,b as D0,j as jn}from"./index-yvgX-7gA.js";/**
+import{r as Oe,b as D0,j as jn}from"./index-LpZb3aPR.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
