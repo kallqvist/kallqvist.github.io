83c7e704e42cbb6f5179fa5bd964d688520095be
kallqvist <kallqvist@gmail.com>
2024-04-06 11:18:06 +0200

    update build

--- assets/index-_TJslMn2.js
+++ assets/index-_TJslMn2.js
@@ -1,4 +1,4 @@
-import{r as Oe,b as D0,j as jn}from"./index-FyvEeBrU.js";/**
+import{r as Oe,b as D0,j as jn}from"./index-gAcSJ6eh.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
