10a41489c9f877a283651d0b8229e95b34d2d0ed
kallqvist <kallqvist@gmail.com>
2024-04-06 19:52:38 +0200

    update build

--- assets/index-nRDwiGYF.js
+++ assets/index-nRDwiGYF.js
@@ -1,4 +1,4 @@
-import{r as Oe,b as D0,j as jn}from"./index-0ahZhjYi.js";/**
+import{r as Oe,b as D0,j as jn}from"./index--6WAjvk0.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
