89582a02453e2aab4ba11ce0ba2195535551ea50
kallqvist <kallqvist@gmail.com>
2024-04-07 21:00:35 +0200

    update build

--- assets/index-BJsIM_L_.js
+++ assets/index-BJsIM_L_.js
@@ -1,4 +1,4 @@
-import{r as Oe,b as D0,j as jn}from"./index-YTe7kLxD.js";/**
+import{r as Oe,b as D0,j as jn}from"./index-Z7Wm_S2L.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
