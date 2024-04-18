34074e2c3545f6cf7044956b75f9519152faf1a6
kallqvist <kallqvist@gmail.com>
2024-04-18 18:54:26 +0200

    update build

--- assets/index-SBy34rgB.js
+++ assets/index-SBy34rgB.js
@@ -1,4 +1,4 @@
-import{r as Oe,b as D0,j as jn}from"./index-v6eTjaTZ.js";/**
+import{r as Oe,b as D0,j as jn}from"./index-uaQOYxUX.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
