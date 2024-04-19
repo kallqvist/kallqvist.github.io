e030179db6606dd9944b59fb10860f916d080c8a
kallqvist <kallqvist@gmail.com>
2024-04-19 09:40:49 +0200

    update build

--- assets/index-6IPkaOuw.js
+++ assets/index-6IPkaOuw.js
@@ -1,4 +1,4 @@
-import{r as Oe,b as D0,j as jn}from"./index-OKwfDJxh.js";/**
+import{r as Oe,b as D0,j as jn}from"./index-0fGeCcMT.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
