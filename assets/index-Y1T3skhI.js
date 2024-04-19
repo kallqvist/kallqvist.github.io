a5276317fd32672f4ea7ff6e367ac132f72832a0
kallqvist <kallqvist@gmail.com>
2024-04-19 18:57:47 +0200

    update build

--- assets/index-Y1T3skhI.js
+++ assets/index-Y1T3skhI.js
@@ -1,4 +1,4 @@
-import{r as Oe,b as D0,j as jn}from"./index-kuqyIrry.js";/**
+import{r as Oe,b as D0,j as jn}from"./index-zDf87eej.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
