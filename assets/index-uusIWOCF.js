f441f418c7b8d41435e5f8608fd09f388b3b4a3c
kallqvist <kallqvist@gmail.com>
2024-04-21 20:55:36 +0200

    update build

--- assets/index-uusIWOCF.js
+++ assets/index-uusIWOCF.js
@@ -1,4 +1,4 @@
-import{r as Oe,b as D0,j as jn}from"./index-Lmnj-Ws3.js";/**
+import{r as Oe,b as D0,j as jn}from"./index-Fyk87sJO.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
