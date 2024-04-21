02fa055e36a54e869d8a8559e5d26ad66bae5823
kallqvist <kallqvist@gmail.com>
2024-04-21 09:18:50 +0200

    update build

--- assets/index-XYtF5sYt.js
+++ assets/index-XYtF5sYt.js
@@ -1,4 +1,4 @@
-import{r as Oe,b as D0,j as jn}from"./index-zDf87eej.js";/**
+import{r as Oe,b as D0,j as jn}from"./index-NepP2luA.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
