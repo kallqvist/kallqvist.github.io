9abfa708fbae4ca7fa351622a7403941d2d8e749
kallqvist <kallqvist@gmail.com>
2024-04-23 19:16:05 +0200

    update build

--- assets/index-k604tQq1.js
+++ assets/index-k604tQq1.js
@@ -1,4 +1,4 @@
-import{r as Oe,b as D0,j as jn}from"./index-dW8MB3Gs.js";/**
+import{r as Oe,b as D0,j as jn}from"./index-djtg_nrC.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
